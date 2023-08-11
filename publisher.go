f37fe30bb7bd48761a7cd646659f0140c17e8cd8
14March <53811984+hcraM41@users.noreply.github.com>
2023-08-11 18:26:10 +0800

    pub
    Co-Authored-By: 14-March <140261121+14-march@users.noreply.github.com>

--- publisher.go
+++ publisher.go
@@ -7,3 +7,65 @@ type Publisher interface {
 
 	Publish(ctx context.Context, event Event) error
 }
+
+type Mapping map[Type]Subscriber
+
+func NewMapping() Mapping {
+	return make(Mapping)
+}
+
+func (pub Mapping) On(typ Type, sub Subscriber) Mapping {
+	if s, ok := pub[typ]; ok {
+		if o, ok := s.(Sync); ok {
+			pub[typ] = append(o, sub)
+		} else {
+			pub[typ] = Sync{s, sub}
+		}
+	} else {
+		pub[typ] = sub
+	}
+	return pub
+}
+
+func (pub Mapping) Publish(ctx context.Context, evt Event) error {
+	if sub, ok := pub[evt.Type()]; ok {
+		return sub.Handle(ctx, evt)
+	}
+	return nil
+}
+
+func (pub Mapping) Handle(ctx context.Context, evt Event) error {
+	return pub.Publish(ctx, evt)
+}
+
+type Buffer struct {
+	publisher Publisher
+	events    []Event
+}
+
+func NewBuffer(pub Publisher) *Buffer {
+	return &Buffer{publisher: pub}
+}
+
+func (pub *Buffer) Publish(ctx context.Context, evt Event) error {
+	pub.events = append(pub.events, evt)
+	return nil
+}
+
+func (pub *Buffer) Handle(ctx context.Context, evt Event) error {
+	return pub.Publish(ctx, evt)
+}
+
+func (pub *Buffer) Dispatch(ctx context.Context) error {
+	var (
+		evt Event
+		err error
+	)
+	for len(pub.events) != 0 {
+		evt, pub.events = pub.events[0], pub.events[1:]
+		if e := pub.publisher.Publish(ctx, evt); e != nil {
+			err = e
+		}
+	}
+	return err
+}
